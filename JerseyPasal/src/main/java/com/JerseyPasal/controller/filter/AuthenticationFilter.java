0b6731da320cf35512979925257a701fc0fe70af
Pankajrai144 <pankajrai0113@gmail.com>
2026-05-15 16:04:29 +0545

    Update AuthenticationFilter.java

--- JerseyPasal/src/main/java/com/JerseyPasal/controller/filter/AuthenticationFilter.java
+++ JerseyPasal/src/main/java/com/JerseyPasal/controller/filter/AuthenticationFilter.java
@@ -19,7 +19,7 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet Filter implementation class AuthenticationFilter
  */
-@WebFilter(urlPatterns = { "/userdashboard", "/admindashboard", "/profile", "/editprofile", "/logout" })
+@WebFilter(urlPatterns = { "/userdashboard", "/admindashboard", "/profile", "/cart", "/editprofile", "/logout" })
 public class AuthenticationFilter extends HttpFilter implements Filter {
 
 	private static final long serialVersionUID = 1L;
