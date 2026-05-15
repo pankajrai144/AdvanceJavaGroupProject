7a28ec1c7d16f59f5cf1fcedf944659ca17c853a
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-05-15 16:12:44 +0545

    Add CartModel class with properties and methods

--- JerseyPasal/src/main/java/com/JerseyPasal/controller/model/CartModel.java
+++ JerseyPasal/src/main/java/com/JerseyPasal/controller/model/CartModel.java
@@ -2,4 +2,66 @@ package com.JerseyPasal.controller.model;
 
 public class CartModel {
 
+    private int cartItemId;
+    private int cartId;
+    private ProductModel product;
+    private int quantity;
+    private String selectedSize;
+
+    public CartModel() {
+    }
+
+    public CartModel(int cartItemId, int cartId, ProductModel product, int quantity, String selectedSize) {
+        this.cartItemId = cartItemId;
+        this.cartId = cartId;
+        this.product = product;
+        this.quantity = quantity;
+        this.selectedSize = selectedSize;
+    }
+
+    public CartModel(ProductModel product, int quantity, String selectedSize) {
+        this.product = product;
+        this.quantity = quantity;
+        this.selectedSize = selectedSize;
+    }
+
+    public int getCartItemId() {
+        return cartItemId;
+    }
+
+    public void setCartItemId(int cartItemId) {
+        this.cartItemId = cartItemId;
+    }
+
+    public int getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+
+    public ProductModel getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductModel product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSelectedSize() {
+        return selectedSize;
+    }
+
+    public void setSelectedSize(String selectedSize) {
+        this.selectedSize = selectedSize;
+    }
 }
