a7b880ff5a95e2e6b81c2ebc2035c00786960468
Pankajrai144 <pankajrai0113@gmail.com>
2026-05-15 22:49:16 +0545

    Update OrderModel.java

--- JerseyPasal/src/main/java/com/JerseyPasal/controller/model/OrderModel.java
+++ JerseyPasal/src/main/java/com/JerseyPasal/controller/model/OrderModel.java
@@ -2,4 +2,66 @@ package com.JerseyPasal.controller.model;
 
 public class OrderModel {
 
+    private int orderId;
+    private int userId;
+    private double orderTotal;
+    private String orderStatus;
+    private String orderDate;
+
+    public OrderModel() {
+    }
+
+    public OrderModel(int orderId, int userId, double orderTotal, String orderStatus, String orderDate) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.orderTotal = orderTotal;
+        this.orderStatus = orderStatus;
+        this.orderDate = orderDate;
+    }
+
+    public OrderModel(int userId, double orderTotal, String orderStatus) {
+        this.userId = userId;
+        this.orderTotal = orderTotal;
+        this.orderStatus = orderStatus;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
 }
