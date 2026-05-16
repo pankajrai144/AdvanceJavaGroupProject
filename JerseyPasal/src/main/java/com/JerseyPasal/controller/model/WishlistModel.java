eeebf6163d8d03e38097ea2dbde84b74e5ed9255
Manisha Shah <ms9809102903@gmail.com>
2026-05-16 09:04:02 +0545

    Update WishlistModel.java

--- JerseyPasal/src/main/java/com/JerseyPasal/controller/model/WishlistModel.java
+++ JerseyPasal/src/main/java/com/JerseyPasal/controller/model/WishlistModel.java
@@ -2,4 +2,66 @@ package com.JerseyPasal.controller.model;
 
 public class WishlistModel {
 
+    private int wishlistId;
+    private int userId;
+    private int productId;
+    private ProductModel product;
+    private String addedAt;
+
+    public WishlistModel() {
+    }
+
+    public WishlistModel(int wishlistId, int userId, int productId, String addedAt) {
+        this.wishlistId = wishlistId;
+        this.userId = userId;
+        this.productId = productId;
+        this.addedAt = addedAt;
+    }
+
+    public WishlistModel(int wishlistId, int userId, ProductModel product, String addedAt) {
+        this.wishlistId = wishlistId;
+        this.userId = userId;
+        this.product = product;
+        this.addedAt = addedAt;
+    }
+
+    public int getWishlistId() {
+        return wishlistId;
+    }
+
+    public void setWishlistId(int wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public ProductModel getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductModel product) {
+        this.product = product;
+    }
+
+    public String getAddedAt() {
+        return addedAt;
+    }
+
+    public void setAddedAt(String addedAt) {
+        this.addedAt = addedAt;
+    }
 }
